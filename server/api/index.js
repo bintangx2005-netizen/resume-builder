b20151528d93fb12d7ebed9b5b71009b55cca7a4
bintangx2005-netizen <bintangx2005@gmail.com>
2025-11-29 22:31:18 +0700

    serverless.http

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,10 @@
-import app from "../server.js";
+import app from "../server.js";     // sesuaikan jika file kamu bernama lain
+import serverless from "serverless-http";
 
-export default function handler(req, res) {
-  app(req, res);
-}
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default serverless(app);
