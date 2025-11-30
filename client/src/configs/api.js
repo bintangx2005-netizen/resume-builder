871c2497b6ceca91ddc046392d57d3941987d057
bintangx2005-netizen <bintangx2005@gmail.com>
2025-11-30 18:21:20 +0700

    123

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL + '/api', // **Hanya domain, tanpa /api/users**
+    baseURL: import.meta.env.VITE_BASE_URL + "/api", // **Hanya domain, tanpa /api/users**
     withCredentials: true
 });
 
