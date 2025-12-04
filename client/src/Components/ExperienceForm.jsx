01bbd0172edd08ee4ce4b4b666bb66594f3adb6b
bintangx2005-netizen <bintangx2005@gmail.com>
2025-12-04 15:20:19 +0700

    Update ExperienceForm.jsx

--- client/src/Components/ExperienceForm.jsx
+++ client/src/Components/ExperienceForm.jsx
@@ -30,20 +30,24 @@ const ExperienceForm = ({ data, onChange}) => {
     const updated = [...data];
     const current = { ...updated[index], [field]: value };
 
-    // VALIDASI: Start date tidak boleh lebih besar dari end date
-    if (!current.is_current && current.start_date && current.end_date) {
+        // VALIDASI HANYA UNTUK TANGGAL
+    if ((field === "start_date" || field === "end_date") && 
+        !current.is_current &&
+        current.start_date &&
+        current.end_date
+    ) {
         const start = new Date(current.start_date);
         const end = new Date(current.end_date);
 
         if (start > end) {
             toast.error("Start date cannot be later than end date.");
-            return; // hentikan update
+            return;
         }
     }
 
-    updated[index] = current;
-    onChange(updated);
-};
+        updated[index] = current;
+        onChange(updated);
+    };
 
 
     const generateDescription = async (index) => {
